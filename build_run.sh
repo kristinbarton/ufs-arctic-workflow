44d5da3272092afdfbcee23f1afd2bcf4e13caa1
Kristin Barton <kristin.barton@noaa.gov>
2025-12-09 19:36:30 +0000

    Allow run directory to be specified.

--- build_run.sh
+++ build_run.sh
@@ -3,27 +3,34 @@ SYSTEM="ursa"
 COMPILER="intelllvm"
 ATM_RES='C185'
 FIX_DIR="/scratch4/BMC/ufs-artic/Kristin.Barton/files/ufs_arctic_development/fix_files"
+RUN_DIR="/scratch4/BMC/ufs-artic/Kristin.Barton/stmp/test_runs"
 export ACCT='ufs-artic'
 
 TOP_DIR=$(pwd)
 UFS_DIR=${TOP_DIR}/ufs-weather-model
 cd ${UFS_DIR}/build
-make clean
+#make clean
 cd ${UFS_DIR}
 
-module purge
-module use modulefiles
-module load ufs_${SYSTEM}.${COMPILER}.lua
-CMAKE_FLAGS="-DAPP=S2S -DREGIONAL_MOM6=ON -DMOVING_NEST=OFF -DCCPP_SUITES=FV3_GFS_v17_coupled_p8_ugwpv1,FV3_HAFS_v1_gfdlmp_tedmf_nonsst" ./build.sh
+#module purge
+#module use modulefiles
+#module load ufs_${SYSTEM}.${COMPILER}.lua
+#CMAKE_FLAGS="-DAPP=S2S -DREGIONAL_MOM6=ON -DMOVING_NEST=OFF -DCCPP_SUITES=FV3_GFS_v17_coupled_p8_ugwpv1,FV3_HAFS_v1_gfdlmp_tedmf_nonsst" ./build.sh
 
 PREP_DIR=${TOP_DIR}/prep
 cd ${PREP_DIR}
-./run_prep.sh --clean --all
-
+#./run_prep.sh --clean --all
 cd ${TOP_DIR}
-mkdir ${TOP_DIR}/run
-MODEL_DIR=${TOP_DIR}/run
 
+base=${RUN_DIR}/${CASE_NAME}
+count=1
+MODEL_DIR=${base}
+while [ -e "$MODEL_DIR" ]; do
+    MODEL_DIR="${base}_${count}"
+    ((count++))
+done
+
+mkdir ${MODEL_DIR}
 mkdir ${MODEL_DIR}/INPUT
 mkdir ${MODEL_DIR}/OUTPUT
 mkdir ${MODEL_DIR}/RESTART
@@ -43,3 +50,5 @@ cp -P ${FIX_DIR}/datasets/* ${MODEL_DIR}/.
 cp -P ${UFS_DIR}/modulefiles/ufs_${SYSTEM}.${COMPILER}.lua ${MODEL_DIR}/modulefiles/modules.fv3.lua
 cp -P ${UFS_DIR}/modulefiles/ufs_common.lua ${MODEL_DIR}/modulefiles/.
 cp -P ${UFS_DIR}/build/ufs_model ${MODEL_DIR}/fv3.exe
+
+echo "Model run directory built in ${MODEL_DIR}"
